ef6ce217add68daa15911764110621efb42a4ac3
Haf <haf@protonmail.ch>
2021-12-20 21:27:21 +0100

    new autoplay works but it's oblivious of any user action

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
   <div class="vh-100 row justify-content-center align-items-center">
     <the-quote-box 
       :currentQuote="currentQuote"
-      :autoplay="autoplay"
+      :autoplay="autoplay.enabled"
       @requestRandom="requestRandom"
       @requestQuery="requestQuery"
       @requestFavorite="requestFavorite"
-      @toggleAutoplay="toggleAutoplay"
+      @toggleAutoplay="autoplay.toggle()"
       class="col col-sm-8 col-lg-6"
     ></the-quote-box>
   </div>
@@ -38,67 +38,41 @@ import { DebugMessage } from '@/debug/debug-message';
 import { useStore } from '@/store';
 import { storeToRefs } from 'pinia';
 import { randomElement } from '@/random-element';
+import { Autoplay } from '@/autoplay';
 
 const currentQuote = ref<Quote | undefined>(undefined);
 const debugLog = reactive(new RingBuffer<DebugMessage>(15));
 const { expertMode, provider, favoriteQuotes } = storeToRefs(useStore());
-const autoplay = ref(true);
-let timeout: number | undefined = undefined;
+const autoplay = reactive(new Autoplay(provider, updateQuote));
 
-function resetTimeout(quoteLength: number) {
-  if (timeout)
-    clearTimeout(timeout);
-  timeout = setTimeout(() => updateQuote(provider.value.random()), 7000 + (quoteLength * 10));
-}
 
-function updateQuote(promisedQuote: Promise<Quote>) {
-  promisedQuote.then(newQuote => {
-    debugLog.add(new DebugMessage('received', newQuote));
-    currentQuote.value = newQuote;
-    if (autoplay.value) {
-      resetTimeout(newQuote.contents.length);
-    }
-  });
+function updateQuote(newQuote: Quote) {
+  debugLog.add(new DebugMessage('received', newQuote));
+  currentQuote.value = newQuote;
 }
 
 function requestRandom() {
   debugLog.add(new DebugMessage('requested'));
-  updateQuote(provider.value.random());
+  provider.value.random().then(updateQuote);
 }
 
 function requestFavorite() {
   const randomFavoriteId = randomElement(Array.from(favoriteQuotes.value));
   debugLog.add(new DebugMessage('requested'));
-  updateQuote(provider.value.byId(randomFavoriteId));
+  provider.value.byId(randomFavoriteId).then(updateQuote);
 }
 
 function requestQuery(requestType: RequestType, query: string) {
   debugLog.add(new DebugMessage('requested', { [requestType]: query }));
   if (requestType === 'author')
-    updateQuote(provider.value.byAuthor(query));
+    provider.value.byAuthor(query).then(updateQuote);
   else if (requestType === 'tag')
-    updateQuote(provider.value.byTag(query));
+    provider.value.byTag(query).then(updateQuote);
   else if (requestType === 'work')
-    updateQuote(provider.value.byWork(query));
+    provider.value.byWork(query).then(updateQuote);
 }
 
-function toggleAutoplay() {
-  const nextAutoplayState = !autoplay.value;
-
-  if (nextAutoplayState) {
-    requestRandom();
-  } else {
-    clearTimeout(timeout);
-  }
-
-  autoplay.value = nextAutoplayState;
-}
-
-onMounted(() => {
-  requestRandom();
-});
-
-onUnmounted(() => clearTimeout(timeout));
+onUnmounted(() => autoplay.stop());
 </script>
 
 <style>
--- src/autoplay.ts
+++ src/autoplay.ts
@@ -2,20 +2,50 @@ import { Ref } from 'vue';
 import { QuoteProvider } from '@/quotes/quote-provider';
 import { Quote } from '@/quotes/quote';
 
-// TODO: Finish this
+const MINIMUM_TIMEOUT = 7000;
 
 export class Autoplay {
   public enabled: boolean;
-  private intervalId: number | undefined;
+  private timeoutId: number | undefined;
   
   constructor(
     private readonly provider: Ref<QuoteProvider>,
     private readonly updateQuote: (quote: Quote) => void,
   ) {
     this.enabled = true; 
-    provider.value.random().then((quote) => {
-      updateQuote(quote);
-      this.intervalId = setTimeout(() => updateQuote(provider.value.random()), 7000 + (quote.contents.length * 10));
-    });
+    this.start();
+  }
+
+  public toggle(): void {
+    if (this.enabled) {
+      this.stop();
+    } else {
+      this.start(); 
+    }
+
+    this.enabled = !this.enabled;
+  }
+
+  public stop(): void {
+    clearTimeout(this.timeoutId);
+  }
+
+  public start(): void {
+    this.provider.value.random().then(this.resetTimeout.bind(this));
+  }
+
+  public resetTimeout(newQuote: Quote): void {
+    this.updateQuote(newQuote);
+    if (this.enabled) {
+      this.timeoutId = setTimeout(
+        () => this.provider.value.random().then(this.resetTimeout.bind(this)),
+        this.calculateTimeoutDuration(newQuote)
+      );
+    }
+  }
+
+  // So quotes stay longer the longer they are, and time is enough to read them
+  private calculateTimeoutDuration(quote: Quote) {
+    return MINIMUM_TIMEOUT + (quote.contents.length * 10);
   }
 }

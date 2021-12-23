1c0e95c133476ea35b819215af9cccb50fb1a4e5
Haf <haf@protonmail.ch>
2021-12-23 19:33:23 +0100

    error when requesting something that doesn't exist

--- src/quotes/json-quote-provider.ts
+++ src/quotes/json-quote-provider.ts
@@ -4,6 +4,13 @@ import { RequestType } from '@/request-type';
 import { randomElement } from '@/random-element';
 import sampleQuotes from './sample-quotes.json';
 
+function checkNotEmpty(quotes: Quote[]): Quote[] {
+  if (quotes.length === 0) {
+    throw new Error("No quote found matching that criteria");
+  }
+  return quotes;
+}
+
 export class JsonQuoteProvider implements QuoteProvider {
   public readonly type: ProviderType = ProviderType.embedded;
 
@@ -13,20 +20,19 @@ export class JsonQuoteProvider implements QuoteProvider {
     this.quotes = Promise.resolve(sampleQuotes);
   }
 
-  // TODO: Error if not found?
   request(type: 'random'): Promise<Quote>;
   request(type: 'author' | 'tag' | 'work' | 'id', query: string): Promise<Quote>;
   async request(type: any, query?: any): Promise<Quote> {
     if (type === 'random')
-      return this.quotes.then(quotes => randomElement(quotes));
+      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes)));
     else if (type === 'author') {
-      return this.quotes.then(quotes => randomElement(quotes.filter(q => q.author === query)));
+      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.author === query))));
     } else if (type === 'work') {
-      return this.quotes.then(quotes => randomElement(quotes.filter(q => q.work === query)));
+      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.work === query))));
     } else if (type === 'tag') {
-      return this.quotes.then(quotes => randomElement(quotes.filter(q => q.tags.includes(query))));
+      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.tags.includes(query)))));
     } else if (type === 'id') {
-      return this.quotes.then(quotes => quotes.filter(q => q.id === query)[0]);
+      return this.quotes.then(quotes => checkNotEmpty(quotes.filter(q => q.id === query))[0]);
     } else {
       throw new Error();
     }

d876031ed7d6ac3216b2943ac857f1b0095f4ff7
Haf <haf@protonmail.ch>
2021-12-26 17:32:24 +0100

    removed unnecesary promise

--- src/quotes/json-quote-provider.ts
+++ src/quotes/json-quote-provider.ts
@@ -1,6 +1,5 @@
 import { Quote } from './quote';
 import { QuoteProvider, ProviderType } from './quote-provider';
-import { RequestType } from '@/request-type';
 import { randomElement } from '@/random-element';
 import sampleQuotes from './sample-quotes.json';
 
@@ -14,25 +13,25 @@ function checkNotEmpty(quotes: Quote[]): Quote[] {
 export class JsonQuoteProvider implements QuoteProvider {
   public readonly type: ProviderType = ProviderType.embedded;
 
-  private quotes: Promise<Quote[]>;
+  private quotes: Quote[];
 
   constructor() {
-    this.quotes = Promise.resolve(sampleQuotes);
+    this.quotes = sampleQuotes;
   }
 
   request(type: 'random'): Promise<Quote>;
   request(type: 'author' | 'tag' | 'work' | 'id', query: string): Promise<Quote>;
   async request(type: any, query?: any): Promise<Quote> {
     if (type === 'random')
-      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes)));
+      return randomElement(checkNotEmpty(this.quotes));
     else if (type === 'author') {
-      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.author === query))));
+      return randomElement(checkNotEmpty(this.quotes.filter(q => q.author === query)));
     } else if (type === 'work') {
-      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.work === query))));
+      return randomElement(checkNotEmpty(this.quotes.filter(q => q.work === query)));
     } else if (type === 'tag') {
-      return this.quotes.then(quotes => randomElement(checkNotEmpty(quotes.filter(q => q.tags.includes(query)))));
+      return randomElement(checkNotEmpty(this.quotes.filter(q => q.tags.includes(query))));
     } else if (type === 'id') {
-      return this.quotes.then(quotes => checkNotEmpty(quotes.filter(q => q.id === query))[0]);
+      return checkNotEmpty(this.quotes.filter(q => q.id === query))[0];
     } else {
       throw new Error();
     }

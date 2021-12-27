4f60bc09a2d252fe9a25ca0929b10ede827bda1e
Haf <haf@protonmail.ch>
2021-12-27 16:37:55 +0100

    testing App.vue

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -2,20 +2,33 @@
 import { mount } from '@vue/test-utils';
 import App from '@/App.vue';
 import { createTestingPinia } from '@pinia/testing';
+import { QuoteProvider } from '@/quotes/quote-provider';
+import { mock } from 'jest-mock-extended';
 import { useStore } from '@/store';
+
+const mockProvider = mock<QuoteProvider>();
 
 function createWrapper() {
   return mount(App, {
     global: {
       plugins: [ createTestingPinia() ],
+      stubs: [ 'font-awesome-icon' ],
     },
-    stubs: [ 'font-awesome-icon' ],
   });
 }
-
 
 describe('App.vue', () => {
   test("it doesn't blow up when mounted", () => {
     const wrapper = createWrapper();
+  });
+
+  // TODO: Fix it
+  test("requests when provider is changed", () => {
+    const wrapper = createWrapper();
+    const store = useStore();
+    store.provider = mockProvider;
+
+    wrapper.vm.$emit('changeProvider');
+    expect(mockProvider.request).toHaveBeenCalledOnce();
   });
 });
